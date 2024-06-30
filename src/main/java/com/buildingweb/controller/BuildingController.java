7f0980e79813207eb8399c6dd89d428bc01e26f0
daoduyquy321323 <daoduyquylop97@gmail.com>
2024-06-30 23:54:00 +0700

    update project

--- src/main/java/com/buildingweb/controller/BuildingController.java
+++ src/main/java/com/buildingweb/controller/BuildingController.java
@@ -1,5 +1,7 @@
 package com.buildingweb.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +13,24 @@ import com.buildingweb.service.BuildingService;
 
 import lombok.RequiredArgsConstructor;
 
+
 @RestController
 @RequiredArgsConstructor
+@PropertySource("classpath:application.properties") // lấy biến từ file cấu hình application.properties
 public class BuildingController {
     private final BuildingService buildingService;
+
+    @Value("${dev.duyquy321323}") // lấy giá trị của biến có key là dev.duyquy321323
+    private String myName;
 
     @GetMapping("/api/building")
     public ResponseEntity<?> getBuilding(@RequestBody BuildingRequest buildingRequest){
         return ResponseEntity.status(HttpStatus.OK).body(buildingService.searchBuildingByBuildingRequest(buildingRequest));
     }
+
+    @GetMapping("/api/myname")
+    public String getMethodName() {
+        return myName;
+    }
+    
 }
